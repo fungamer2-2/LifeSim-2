a7df96178435a538d9c64fae6aab2eb8e59b4d2f
fungamer2-2 <56766520+fungamer2-2@users.noreply.github.com>
2021-01-06 12:53:36 -0500

    Update IllnessClass.py

--- src/IllnessClass.py
+++ src/IllnessClass.py
@@ -1,7 +1,8 @@
 class Illness:
-    def __init__(self, name, healthDecay, happinessDecay, cureChance, cureCost):
+    def __init__(self, name, healthDecay, happinessDecay, intelligenceDecay, cureChance, cureCost):
         self.name = name
         self.healthDecay = healthDecay
         self.happinessDecay = happinessDecay
+        self.intelligenceDecay = intelligenceDecay
         self.cureChance = cureChance
         self.cureCost = cureCost
